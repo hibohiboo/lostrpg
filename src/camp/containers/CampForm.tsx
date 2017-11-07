f24d3718555892293a98b13294e2e7c945d8ee6b
hibohiboo <swdice66@gmail.com>
2017-11-07 22:47:33 +0900

    バリデーション

--- src/camp/containers/CampForm.tsx
+++ src/camp/containers/CampForm.tsx
@@ -2,13 +2,42 @@ import * as React from 'react'
 import {Component} from 'react'
 import { Field, reduxForm } from 'redux-form'
 
+interface IErrors{
+  campName?: string;
+}
+
+const validate = values => {
+  const errors:IErrors = {}
+  if (!values.campName) {
+    errors.campName = 'Required'
+  }
+  return errors
+}
+
+const renderField = ({
+  input,
+  label,
+  type,
+  meta: { touched, error, warning }
+}) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type} />
+      {touched &&
+        ((error && <span>{error}</span>) ||
+          (warning && <span>{warning}</span>))}
+    </div>
+  </div>
+)
+
 const FormComponent = props => {
     const { handleSubmit } = props
     return (
       <form onSubmit={ handleSubmit }>
         <div>
           <label htmlFor="campName">キャンプ名</label>
-          <Field name="campName" component="input" type="text" />
+          <Field name="campName" component={renderField} type="text" />
         </div>
         <button type="submit">Submit</button>
       </form>
@@ -16,7 +45,8 @@ const FormComponent = props => {
   }
   
   const CampForm = reduxForm({
-    form: 'camp'
+    form: 'camp',
+    validate
   })(FormComponent);
   
   export default CampForm;
