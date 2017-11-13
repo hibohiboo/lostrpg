3bb7e8dd4254cc7acbadc9c0994005159d00c090
hibohiboo <swdice66@gmail.com>
2017-11-13 21:24:58 +0900

    tslint

--- src/camp/app.tsx
+++ src/camp/app.tsx
@@ -1,19 +1,19 @@
+import { User } from 'firebase';
 import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import App from './components/App';
+import { twitterLogin } from './firebase/login';
 import { fetchStart as addFetch } from './modules/addFacility';
 import { fetchStart } from './modules/load';
 import store from './store';
-import {twitterLogin} from './firebase/login';
-
 twitterLogin(viewEdit);
 
-function viewEdit(){
+function viewEdit(user: User) {
   store.dispatch(fetchStart('blankCamp.json'));
   store.dispatch(addFetch('additionalFacilities.json'));
-  
+
   render(
     // providerにstoreは必須
     <Provider store={store}>
@@ -21,5 +21,5 @@ function viewEdit(){
     </Provider>,
     document.getElementById('app'),
     null,
-  );  
+  );
 }
