bca1432149981dcbd17e429cf03222678441b4dc
hibohiboo <swdice66@gmail.com>
2018-08-19 18:32:23 +0900

    不要な過去のelm-spaを削除

---
+++ src/elm-singles/native/index.js
@@ -0,0 +1,18 @@
+var app = Elm.Main.fullscreen(localStorage.session || null);
+
+app.ports.storeSession.subscribe(function(session) {
+  localStorage.session = session;
+});
+
+window.addEventListener("storage", function(event) {
+  if (event.storageArea === localStorage && event.key === "session") {
+    app.ports.onSessionChange.send(event.newValue);
+  }
+}, false);
+
+import {redirectTwitter} from './Firebase/fb';
+
+//ElmからJSへはsubscribe
+app.ports.redirectTwitter.subscribe(function() {
+  redirectTwitter();
+});
